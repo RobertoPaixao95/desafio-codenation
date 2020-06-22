415b1d39e94f8868355a86dc595c64cd2b9670bc
Roberto <robertocsp95@gmail.com>
2020-06-22 09:24:12 -0300

    Exclui package.json

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="codenation_challenge",
-    version="1.0.0",
-    description="Pacote de resolução do Desafio Codenation DS",
-    packages=["ds_cn_challenge"],
-    install_requires=["pandas", "scikit-learn"],
-)
